9b6fde41202bb80d0eb665ab6dc23027609dc294
Konstantine Tavadze <tavadze@nomachine.com>
2022-11-07 23:59:41 +0400

    added sign-up controller and route, started working on get user func

--- Backend/Routes/users.route.js
+++ Backend/Routes/users.route.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("testman");
-});
+const user_controller = require('../Controllers/user.controller')
+
+router.post("/api/auth/sign-up", user_controller.user_register)
+router.get("/api/auth/me", user_controller.get_user)
 
 module.exports = router;
